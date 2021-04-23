1a437481a9f0cb63a4f3878ff31ea0640736793a
Beth <bethm.meeker@gmail.com>
2021-04-23 12:47:40 -0600

    Pull out individual functionality as single methods in Game.js. Also sorted out Icon issues within index.html and styles.css

--- main.js
+++ main.js
@@ -1,59 +1,92 @@
 // DOM VARIABLES
+  // containers
+var chooseScreen = document.getElementById('chooseContainer');
+var difficultGame = document.getElementById('difficultGame');
+var classicGame = document.getElementById('classicGame');
+
+  //buttons
 var classicBtn = document.getElementById('classicBtn');
+var difficultBtn = document.getElementById('difficultBtn');
+
+  // icons
+var rock = document.getElementById('rockIcon');
+var paper = document.getElementById('paperIcon');
+var scissors = document.getElementById('scissorsIcon');
+var turtle = document.getElementById('turtleIcon');
+var pizza = document.getElementById('pizzaIcon');
+var sewer = document.getElementById('sewerIcon');
+var mic = document.getElementById('microphoneIcon');
+var ninjaStar = document.getElementById('ninjaStarIcon');
+var topRowIcons = document.getElementById('topRowIcons');
+var bottomRowIcons = document.getElementById('bottomRowIcons');
+
+
 // GLOBAL VARIABLES
 var newGame;
-var playerStats = [];
+
 
 // EVENT LISTENERS
 classicBtn.addEventListener('click', startClassicGame)
+difficultBtn.addEventListener('click', startDifficultGame)
 
 // EVENT HANDLERS
 
 //master game functions
 function startClassicGame() {
-  createNewClassicGame();
-  loadClassicWins();
-  newGame.human.name = 'Human';
-  newGame.computer.name = 'Machine';
-  newGame.human.token = '🧠';
-  newGame.computer.token = '🤖';
+  createGame();
+  newGame.gameType();
+  newGame.loadPlayerNames();
+  newGame.loadTokens()
+  newGame.loadWins();
+  changeToGameScreen();
   console.log(newGame);
-}
-
-function createNewClassicGame() {
-  newGame = new Game({human: new Player(), computer: new Player(), type: 'Classic'});
-}
-
-function loadClassicWins() {
-  if (localStorage === null) {
-    newGame.human.wins = 0;
-    newGame.computer.wins = 0;
-    console.log('no storage!');
-  }
 }
 
 function startDifficultGame() {
-  newGame = new Game({human: new Player(), computer: new Player(), type: 'Difficult'});
-  newGame.human.name = 'Human';
-  newGame.computer.name = 'Machine';
-  newGame.human.token = '🧠';
-  newGame.computer.token = '🤖';
+  createGame();
+  newGame.gameType();
+  newGame.loadPlayerNames();
+  newGame.loadTokens();
+  newGame.loadWins();
+  changeToGameScreen();
   console.log(newGame);
 }
 
+function createGame() {
+  newGame = new Game({human: new Player(), computer: new Player()})
+}
 
+function changeToGameScreen() {
+  if (newGame.type === 'Classic') {
+    hideElement(chooseScreen);
+    unhideElement(topRowIcons);
+    toggleDiffIcons(turtle, pizza, sewer, mic, ninjaStar);
+  } else {
+    hideElement(chooseScreen);
+    unhideElement(topRowIcons);
+    unhideElement(bottomRowIcons);
+    toggleClassicIcons(rock, paper, scissors);
+  }
+}
 
 // toggle and hide functions
 function hideElement(element) {
-  element.classList.add('hidden');
+  element.classList.toggle('hidden');
 }
 
 function unhideElement(element) {
-  element.classList.remove('hidden');
+  element.classList.toggle('hidden');
 }
 
-function toggleIcons(icon1, icon2, icon3) {
+function toggleClassicIcons(icon1, icon2, icon3) {
   icon1.classList.toggle('hidden');
   icon2.classList.toggle('hidden');
   icon3.classList.toggle('hidden');
+}
+function toggleDiffIcons(icon1, icon2, icon3, icon4, icon5) {
+  icon1.classList.toggle('hidden');
+  icon2.classList.toggle('hidden');
+  icon3.classList.toggle('hidden');
+  icon4.classList.toggle('hidden');
+  icon5.classList.toggle('hidden');
 }
